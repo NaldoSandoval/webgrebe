1ef33d8ed1ba287e69e4d461c4e856be584bf971
hernaldo@gmail.com <hernaldo@gmail.com>
2025-01-14 20:55:21 -0300

    advancing with responsive reel

--- js/script.js
+++ js/script.js
@@ -36,37 +36,6 @@ document.addEventListener("DOMContentLoaded", function () {
         updateImages();
     });
 
-    // Function to integrate LibreTranslate API
-    // const translateText = async (text, targetLanguage) => {
-    //     const response = await fetch('https://libretranslate.com/translate', {
-    //         method: 'POST',
-    //         headers: { 'Content-Type': 'application/json' },
-    //         body: JSON.stringify({
-    //             q: text,
-    //             source: 'en',
-    //             target: targetLanguage,
-    //             format: 'text',
-    //         }),
-    //     });
-    //     const data = await response.json();
-    //     return data.translatedText;
-    // };
-
-    // const updateContentLanguage = async (targetLanguage) => {
-    //     const elementsToTranslate = document.querySelectorAll("[data-translate]");
-    //     for (const element of elementsToTranslate) {
-    //         const originalText = element.dataset.translate; // Get the data-translate attribute
-    //         if (originalText) {
-    //             const translatedText = targetLanguage === 'en'
-    //                 ? originalText // Use the original text for English
-    //                 : await translateText(originalText, targetLanguage);
-    //             element.innerText = translatedText; // Replace text content with the translation
-    //         } else {
-    //             console.error('Missing data-translate attribute on:', element);
-    //         }
-    //     }
-    // };
-
     // Language switcher: English
     document.getElementById('en').addEventListener('click', async () => {
         const enButton = document.getElementById('en');
